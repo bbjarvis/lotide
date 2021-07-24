c36d314edc3c23a7d80bff834a37cb88527163fa
vagrant <bbjarvis>
2021-07-24 22:38:04 +0000

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,25 @@
+//fucntion implementation
+const assertEqual = function(actual, expected) {
+  const pass = '✔️✔️✔️ Assertion Passed';
+  const fail = '❌❌❌ Assertion Failed';
+  if (actual === expected) {
+    if (typeof actual === "string") {
+      console.log(pass + ': "' + actual + '" === "' + expected + '"');
+    } else {
+      console.log(pass + ": " + actual + " === " + expected);
+    }
+  } else {
+    if (typeof actual === "string") {
+      console.log(fail + ': "' + actual + '" !=== "' + expected + '"');
+    } else {
+      console.log(fail + ": " + actual + " !=== " + expected);
+    }
+  }
+};
+
+
+//test code
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
