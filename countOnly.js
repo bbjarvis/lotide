b25f6bf00da6989d1d8021a5d4a89fd09c336e34
Brett Jarvis <72511857+bbjarvis@users.noreply.github.com>
2021-08-01 16:28:30 -0700

    Update countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -15,17 +15,44 @@ const assertArraysEqual = function(array1, array2) {
   const fail = '❌❌❌ Assertion Failed';
   eqArrays(array1, array2) ? console.log(pass) : console.log(fail);
 };
+/*  function takes in two args and checkes if they are equal, returns message:
+    "Assertion Passed: [actual] === [expected]" or
+    "Assertion Failed: [actual] !=== [expected]"
+    also checks if the arg is string and if so returns the message along with the string
+    quotation marks.
+*/
+const assertEqual = function(actual, expected) {
+  const pass = '✔️✔️✔️ Assertion Passed';
+  const fail = '❌❌❌ Assertion Failed';
+  // added check for string, as the example had quotation marks for strings
+  if (actual === expected) {
+    if (typeof actual === "string") {
+      console.log(`${pass}: "${actual}" === "${expected}"`);
+    } else {
+      console.log(`${pass}: ${actual} === ${expected}`);
+    }
+  } else {
+    if (typeof actual === "string") {
+      console.log(`${fail}: "${actual}" !=== "${expected}"`);
+    } else {
+      console.log(`${fail}: ${actual} !=== ${expected}`);
+    }
+  }
+};
 /*  Takes an array and an object. 
     Returns an object containing counts of everything that the input object listed 
 */
 const countOnly = function(allItems, itemsToCount) {
-  for (const key in itemsToCount) {
-    if (Object.hasOwnProperty.call(itemsToCount, key)) {
-      const element = itemsToCount[key];
-      
-      
-    }
+  let results = {};
+
+      for (const item of allItems) {     
+        for (const key in itemsToCount) {
+          if ((key === item) && itemsToCount[key]) {
+            results.hasOwnProperty(item) ? results[key] += 1 : results[key] = 1
+          }   
+      }    
   }
+  return results;
 };
 
 const firstNames = [
