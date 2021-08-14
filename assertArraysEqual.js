434e922988474c54421e6bb4986e7c04645183f0
bbjarvs <brettbjarvis@gmail.com>
2021-08-14 15:55:37 +0000

    refactoring assertArraysEqual.js to seperate testing

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-/* compare two arrays and return "true" or "false" if perfect match
- */
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 /*  Compares 2  arrays using "eqArrays" and return a pass/fail
     message
@@ -21,7 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   eqArrays(actual, expected) ? console.log(`${pass}: ${actual} === ${expected}`) : console.log(`${fail}: ${actual} !== ${expected}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // fail
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // pass
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // fail
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,5 @@
-/*  function takes in two args and checkes if they are equal, returns message:
-    "Assertion Passed: [actual] === [expected]" or
-    "Assertion Failed: [actual] !=== [expected]"
-    also checks if the arg is string and if so returns the message along with the string
-    quotation marks.
-*/
-const assertEqual = function(actual, expected) {
-  const pass = '✔️✔️✔️ Assertion Passed';
-  const fail = '❌❌❌ Assertion Failed';
-  if (actual === expected) {
-    if (typeof actual === "string") {
-      console.log(`${pass}: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`${pass}: ${actual} === ${expected}`);
-    }
-  } else {
-    if (typeof actual === "string") {
-      console.log(`${fail}: "${actual}" !=== "${expected}"`);
-    } else {
-      console.log(`${fail}: ${actual} !=== ${expected}`);
-    }
-  }
-};
+const assertEqual = require('./assertEqual');
+
 /* compare two arrays and return "true" or "false" if perfect match
  */ 
  const eqArrays = function(arr1, arr2) {
@@ -34,17 +13,5 @@ const assertEqual = function(actual, expected) {
 	}
 	return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-//console.log("should Pass");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-//console.log("should Pass");
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-//console.log("should Fail");
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-//console.log("should Pass");
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-//console.log("should Fail");
+module.exports = eqArrays;
